08f7cc3b5dacb657a74b6189daf12c7955e39343
JohnNg-GA <john.q.ng@gmail.com>
2025-04-23 09:21:16 -0700

    CARMEN SANDIEGO: Halfway there on her trail

--- find_carmen.sql
+++ find_carmen.sql
@@ -25,19 +25,50 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+-- SELECT * FROM country WHERE region = 'Southern Europe' ORDER BY populations ASC LIMIT 1; ** Search through all countries with Southern Europe regional tag and ordered in ascending size to determine smallest population and limited the search to only show the first country (the lowest population) 
+
+-- OUTPUT:
+-- code |             name              | continent |     region      | surfacearea | indepyear | population | lifeexpectancy | gnp  | gnpold |            localname            |      governmentform      |     headofstate     | capital | code2
+------+-------------------------------+-----------+-----------------+-------------+-----------+------------+----------------+------+--------+---------------------------------+--------------------------+---------------------+---------+-------
+--  VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                | 9.00 |        | Santa Sede/Cittï¿½ del Vaticano | Independent Church State | Johannes Paavali II |    3538 | VA
+
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
+-- SELECT * FROM countrylanguage; ** Used code to determine how countries are identified in 'countrylanguage' table to see what is similar between this and the 'country' table
+-- SELECT * FROM countrylanguage WHERE countrycode = 'VAT'; ** With insight to use 'countrycode' as search parameter, determined the official recognized language
+
+-- OUTPUT:
+-- countrycode | language | isofficial | percentage
+-------------+----------+------------+------------
+-- VAT         | Italian  | t          |          0
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
+-- SELECT * FROM countrylanguage WHERE language = 'Italian' ORDER BY percentage DESC LIMIT 1; ** Search parameter to determine what country's percentage of population all speak Italian:
 
+-- countrycode | language | isofficial | percentage
+-------------+----------+------------+------------
+-- SMR         | Italian  | t          |        100
+
+-- SELECT * FROM country WHERE code = 'SMR'; ** search parameter to indicate country details
+
+-- code |    name    | continent |     region      | surfacearea | indepyear | population | lifeexpectancy |  gnp   | gnpold | localname  | governmentform | headofstate | capital | code2
+------+------------+-----------+-----------------+-------------+-----------+------------+----------------+--------+--------+------------+----------------+-------------+---------+-------
+-- SMR  | San Marino | Europe    | Southern Europe |          61 |       885 |      27000 |           81.1 | 510.00 |        | San Marino | Republic       |             |    3171 | SM
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
+-- SELECT * FROM city; ** query through database to see table columns that I can filter from:
+-- OUTPUT:
+--   id  |    name    | countrycode |     district      | population
 
-
+-- SELECT * FROM city WHERE countrycode = 'SMR' AND name != 'San Marino'; ** Query search to determine the cities within San Marino and removed option for country that isn't the same name based on clue (researched using AND statement to join):
+-- OUTPUT:
+--  id  |    name    | countrycode |     district      | population
+------+------------+-------------+-------------------+------------
+-- 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 
 
